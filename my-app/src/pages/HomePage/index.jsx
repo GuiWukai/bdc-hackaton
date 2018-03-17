0dec7f23ddb4918b38f705f7fe1b2df8a77596f4
Guillaume Ah-ki <guillaume_ahki@hotmail.com>
2018-03-17 14:08:57 -0400

    added data

--- my-app/src/pages/HomePage/index.jsx
+++ my-app/src/pages/HomePage/index.jsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { IdInput } from "../../components";
-import patientData from "../../data/patients.json";
+import patients from "../../data/patients.json";
+import genotypes from "../../data/genotypes.json";
+import genes from "../../data/genes.json";
+import phenotypes from "../../data/phenotypes.json";
 
 export class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      patient: {}
+      patient: {
+        _id: "",
+        genotypes: [],
+        nutrigenomics: [],
+        pharmacogenetics: [],
+        name: "",
+        birthday: "",
+        gender: "",
+        __v: ""
+      }
     };
   }
 
-  handleSearch = id => {
-    patientData.forEach(patient => {
-      if (patient._id === id) {
-        console.log(patient);
-        this.setState({
-          patient: patient._id
-        });
-      }
-    });
-  };
   render() {
     console.log(this.state.patient);
+    const { patient } = this.state;
     return (
       <div>
         <IdInput handleSearch={this.handleSearch} />
